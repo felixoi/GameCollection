6e3f171f555d6add3c92a1856d07fa4545924768
Felix Kleinekathöfer <felixoiDev@gmail.com>
2018-10-11 11:05:39 +0200

    Initial commit

---
+++ src/main/java/net/felixoi/gamecollection/GameCollection.java
@@ -0,0 +1,26 @@
+package net.felixoi.gamecollection;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(
+        id = "gamecollection",
+        name = "GameCollection",
+        description = "A nice minigame collection",
+        authors = {
+                "felixoi"
+        }
+)
+public class GameCollection {
+
+    @Inject
+    private Logger logger;
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+        logger.info("Initialized!");
+    }
+}
